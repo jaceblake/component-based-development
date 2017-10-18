a6864a630e887b20c8b145be4281c67e1990f3ab
jahid6847@gmail.com <jahid6847@gmail.com>
2017-10-18 12:30:44 +0200

    created ExternalEmailSyntaxChecker class

--- EmailChecker/src/main/java/de/htw/ws/kbe/EmailChecker/ExternalEmailSyntaxChecker.java
+++ EmailChecker/src/main/java/de/htw/ws/kbe/EmailChecker/ExternalEmailSyntaxChecker.java
@@ -1,6 +1,11 @@
 package de.htw.ws.kbe.EmailChecker;
+import org.apache.commons.validator.routines.*;
 
 public class ExternalEmailSyntaxChecker {
 
+	EmailValidator ev = EmailValidator.getInstance();
 	
+	public boolean isValidEmailAddress(String emailAddress) {
+        return  ev.isValid(emailAddress);
+	}
 }
