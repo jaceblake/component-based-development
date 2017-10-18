72922da791dffbf3e8f676a58ad320c83d9d8409
jahid6847@gmail.com <jahid6847@gmail.com>
2017-10-18 11:11:18 +0200

    implemented EmailChecker function

---
+++ EmailChecker/src/main/java/de/htw/ws/kbe/EmailChecker/MyEmailSyntaxChecker.java
@@ -0,0 +1,39 @@
+package de.htw.ws.kbe.EmailChecker;
+
+public class MyEmailSyntaxChecker {
+
+	
+	public boolean isValidEmailAddress(String emailAddress) {
+		
+		if(emailAddress == null) {
+			return false;
+		}
+		
+		else if(emailAddress.equals("")) {
+			return false;
+		}
+		
+		else if(!emailAddress.contains("@")) {
+			return false;
+		}
+
+		else if(!emailAddress.substring(emailAddress.indexOf("@"), emailAddress.length()-1).contains(".")) {
+			return false;
+		}
+		
+		else if(emailAddress.substring(emailAddress.indexOf("."),emailAddress.length()-1).length()<2) {
+			return false;	
+		}
+		
+		else if(emailAddress.contains(" ")) {
+			return false;
+		}
+		
+		
+		return true;
+		
+	}
+	
+
+	
+}
