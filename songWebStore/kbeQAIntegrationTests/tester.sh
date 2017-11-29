037006d1f087b5fec17c818da7bcf76197f6d2e3
sabirmostofa <sabirmostofa@gmail.com>
2017-11-29 10:33:43 +0100

    Test results

---
+++ songWebStore/kbeQAIntegrationTests/tester.sh
@@ -0,0 +1,91 @@
+#! /bin/sh
+
+HOME_CAT=/home/s0542338/apache-tomcat-8.5.23
+
+echo "--- REQUESTING ALL SONGS ------------"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songWebStore?all"
+echo " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- REQUESTING SONG NUMBER 7 --------"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songWebStore?songId=7"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- POSTING A SONG ------------------"
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -H "Accept: test/plain" \
+     -d "@einSong.json" \
+     -v "http://localhost:8080/songWebStore"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- REQUESTING THE NEW SONG NUMBER --"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songWebStore?songId=11"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- POSTING ANOTHER SONG ------------"
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -H "Accept: test/plain" \
+     -d "@einZweiterSong.json" \
+     -v "http://localhost:8080/songWebStore"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- BAD USER REQUESTS ---------------"
+
+echo "--- GET WITH NON-EXISTING ID ---------------"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songWebStore?songId=22"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- GET WITH BAD ID ---------------"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songWebStore?songId=notANumber"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- GET WITH NO PARAM ---------------"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songWebStore?NO"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- POSTING A NON-JSON LOVE SONG ----"
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -H "Accept: test/plain" \
+     -d "@notALoveSong.txt" \
+     -v "http://localhost:8080/songWebStore"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- POSTING AN EMPTY FILE -----------"
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -H "Accept: test/plain" \
+     -d "@emptyFile.txt" \
+     -v "http://localhost:8080/songWebStore"
+echo = " "
+echo "-------------------------------------------------------------------------------------------------"
+
+echo "--- SHUTTING DOWN TOMCAT -----------"
+$HOME_CAT/bin/shutdown.sh
+
+sleep 1
+
+echo "NUMBER OF SONGS IN DB-FILE: "
+cat $HOME_CAT/webapps/songWebStore/WEB-INF/classes/songs.json | grep \"id\" | wc -l
